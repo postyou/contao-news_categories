c63264245b034e899b4aa0bb3340203094bd4419
Mario Gienapp <mario.gienapp@postyou.de>
2026-08-17 09:16:59 +0200

    Optimize active category check

--- src/FrontendModule/CumulativeHierarchicalFilterModule.php
+++ src/FrontendModule/CumulativeHierarchicalFilterModule.php
@@ -150,7 +150,7 @@ class CumulativeHierarchicalFilterModule extends NewsModule
                 $category->getTitle(),
                 $category->getTitle(),
                 $this->generateItemCssClass($category),
-                null !== $activeCategories && \in_array($category, $activeCategories->getModels(), false),
+                null !== $activeCategories && \in_array((int) $category->id, array_map(intval(...), $activeCategories->fetchEach('id')), true),
                 !$this->showLevel || $this->showLevel >= $level ? $this->renderNewsCategories($category->id, $ids, $level) : '',
                 $category,
             );
